a39e56c66072a12c864b2863b6dc83a02885ba17
JoacoZeba <joacozeba18@gmail.com>
2025-02-14 16:56:08 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
