5082f0e4c4d62a23e6f356ca2b731dfc08618a2c
JoacoZeba <joacozeba18@gmail.com>
2025-02-14 14:41:31 +0000

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    /*se puede definir una var ej: int str1= "...";para utilizar puts(str1) o simplemente utilizar puts("..."); */
+	puts("Programming is like building a multilingual puzzle");
+    return 0;	
+}	
