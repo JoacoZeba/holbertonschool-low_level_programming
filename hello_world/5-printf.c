61a6e8812e5e8d60d51e51a96355508ba930939b
JoacoZeba <joacozeba18@gmail.com>
2025-02-14 16:57:20 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - that prints exactly with proper grammar,
  * but the outcome is a piece of art,, followed by a new line.
- * Return: 0
+ * Return:0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
