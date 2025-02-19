1fc512dee7dee062a1702f4a367f0b90ff81c6c3
JoacoZeba <joacozeba18@gmail.com>
2025-02-19 12:57:58 +0000

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - assign a random number to a variable n each time
  * it is executed
- * Return:0 
+ * Return:0
  */
 int main(void)
 {
@@ -12,6 +12,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+	last_digit = n % 10
+	printf("Last digit of %d is %d and is last_digit", n, last_digit)
+	if ("last_digit > 5")
+		printf("greater than 5")
+	else if (last_digit == 0)
+		printf("ïs 0")
+	else (last_digit < 6)
+		printf("is less than 6 and not 0")
 	return (0);
 }
