9b7d158356e13f9b3eeafbbb059ec5ca9868304d
JoacoZeba <joacozeba18@gmail.com>
2025-02-19 16:02:41 +0000

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-        char alphabet;
-        char ALPHABET;
+	char alphabet;
+	char ALPHABET;
 
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-                putchar(alphabet);
-        }
-        for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
-        {
-                putchar(ALPHABET);
-        }
-        putchar('\n');
-        return (0);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
+	{
+		putchar(ALPHABET);
+	}
+	putchar('\n');
+	return (0);
 }
