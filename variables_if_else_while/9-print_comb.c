c17afe4a4bca38d97b467ef9e6e8e9e0918f13ca
JoacoZeba <joacozeba18@gmail.com>
2025-02-20 18:08:35 +0000

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers separated by a ","
+ * Return:0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+			if (num == '9')
+				putchar('9');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
