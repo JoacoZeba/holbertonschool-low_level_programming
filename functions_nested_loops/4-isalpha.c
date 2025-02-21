4ca461840e0a3922dd186d9001ec1a3c8507c04a
JoacoZeba <joacozeba18@gmail.com>
2025-02-21 17:22:24 +0000

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - cheks if a char is an alphabetic char or not no matter
+ * lower or uppercase
+ * Return:1 if c is an alphabetic char and 0 if its not
+ * @c: char to be checked
+ */
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
