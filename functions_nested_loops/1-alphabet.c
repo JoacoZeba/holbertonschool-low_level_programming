8d53fe953d775bd0d159ffe7faf44439d9188ab1
JoacoZeba <joacozeba18@gmail.com>
2025-02-21 14:55:12 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ */
+void print_alphabet(void)
+{
+	char num;
+	for (num = 'a'; num <= 'z'; num++)
+	{
+		_putchar(num);
+	}
+		_putchar('\n');
+}
