23558bc6c6fdb3cffcf394b9ddf8b6d1d657e634
JoacoZeba <joacozeba18@gmail.com>
2025-02-24 14:34:03 +0000

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - checks if a char is lowercase or not
+ * _isupper - checks if a char is uppercase or not
  * Return:1 if is uppercase 0 if its not
  * @c: char to be checked
  */
@@ -8,7 +8,7 @@ int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-	return (1);
+		return (1);
 	}
 	return (0);
 }
