0e4b0e9b4fb477e1d0b428974274810ecdd9c94b
JoacoZeba <joacozeba18@gmail.com>
2025-03-03 16:33:36 +0000

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a char in a string
+ * @s:string to find the char on
+ * @c: char to be found
+ * Return: a pointer to the char c or NULL if there isnt one
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+		{
+			return (s);
+		}
+	return (NULL);
+}
