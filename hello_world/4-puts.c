71e7432e75e2b763ce4a88ed3f67cea894ae16d3
JoacoZeba <joacozeba18@gmail.com>
2025-06-20 16:40:33 +0000

    task 4 5

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a string with puts
+ * Return: 0 if correct
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
