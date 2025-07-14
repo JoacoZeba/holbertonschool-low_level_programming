de3dad66de4fe8e9ac778718854b5e9cdfd578c2
JoacoZeba <joacozeba18@gmail.com>
2025-07-14 02:42:41 +0000

    .

---
+++ recursion/main.h
@@ -0,0 +1,11 @@
+/*Recursion Functions Prototypes.*/
+
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int n);
+int is_palindrome(char *);
+int wildcmp(char *, char *);
